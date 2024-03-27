95f9a4e7b4a923c6dfb8f94a4b2efbbbecc1a1eb
meni-kr <meni.kramer@gmail.com>
2024-03-27 19:52:52 +0200

    Fix unknown problem in HashRouter

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -14,8 +14,6 @@ export const toyService = {
 const labels = ["On wheels", "Box game", "Art", "Baby", "Doll", "Puzzle", "Outdoor", "Battery Powered"]
 
 function query(filterBy, sort) {
-    console.log('filterBy:',filterBy )
-    console.log('sort:',sort )
     return httpService.get('toy', { params: { filterBy, sort } })
 }
 
